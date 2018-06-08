ce21879c988ddc6d6f5186a18811a80a8daf3f47
Lars <lars@roth-kl.de>
2018-06-08 13:46:03 +0200

    First Push

---
+++ js/functions.js
@@ -0,0 +1,302 @@
+/* Functions for the usermanagement system of the intranet */
+
+var Users;
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+        update();
+    });
+})
+
+function update() {
+    $("#tabs-1").html("");
+    $("#tabs-2").html("");
+    $("#tabs-3").html("");
+    $("#http-status").html("");
+    get_Data();
+}
+
+function get_Data() {
+    Users = null;
+    $.get("http://h2669567.stratoserver.net:8080/intranet/jsonusers.jsp", function(data) {
+        Users = data.users;
+        console.log(Users);
+        if (data.status != "ok") {
+            create_Error(data.status);
+        }
+
+        create_Users();
+
+    });
+}
+
+
+
+function create_Card() {
+    for (var i = 0; i < Users.length; i++) {
+        var main_div = $("<div class='user-box'></div>");
+        var sec_div = $("<div class='card user'>");
+        var third_div = $("<div class='card-body'>");
+        var header = $("<h1 class='card-title'>").text(Users[i].firstname + ' ' + Users[i].lastname);
+        if (Users[i].hasOwnProperty('deletionDate')) {
+            var sub_title1 = $("<h6 class='card-subtitle creation blockify'>").text('Gelöscht am:' + Users[i].deletionDate);
+        } else {
+            var sub_title1 = $("<h6 class='card-subtitle creation blockify'>").text('Erstellt am:' + Users[i].creationDate);
+        }
+        var span1 = $("<span class='deluser'>");
+        var span2 = $("<span class='deluser'>");
+        if (Users[i].hasOwnProperty('deletionDate')) {
+            var react_btn = $("<button class='btn btn-block blockify' onclick='react_User(this)'>").text("Reaktivieren");
+        } else {
+            var del_btn = $("<button class='btn btn-block blockify' onclick='del_User(this)'>").text("Delete");
+            var newpw_btn = $("<button class='btn btn-block blockify' onclick='new_Pass(this)'>").text("New Password");
+        }
+
+
+        switch (Users[i].role) {
+            case 2:
+                var sub_title2 = $("<h6 class='card-subtitle creation role_high blockify'>").text("Administrator");
+                break;
+
+            default:
+                var sub_title2 = $("<h6 class='card-subtitle creation blockify'>").text("Benutzer");
+                break;
+
+        }
+        header.attr('id', Users[i].id);
+        if (!Users[i].hasOwnProperty('deletionDate')) {
+            $("#tabs-1").append(main_div);
+            $(main_div).append(sec_div);
+            $(sec_div).append(third_div);
+            $(third_div).append(header, sub_title1, sub_title2, span1, span2);
+            $(span1).append(del_btn);
+            $(span2).append(newpw_btn);
+        } else {
+            $("#tabs-2").append(main_div);
+            $(main_div).append(sec_div);
+            $(sec_div).append(third_div);
+            $(third_div).append(header, sub_title1, sub_title2, span1, span2);
+            $(span1).append(react_btn);
+        }
+    }
+}
+
+function create_Table(location) {
+    for (var i = 0; i < Users.length; i++) {
+        var tr = $("<tr>");
+        var td1 = $("<td>").text(Users[i].firstname);
+        var td2 = $("<td>").text(Users[i].lastname);
+        switch (location) {
+            case "#tabs-1":
+                var td3 = $("<td>").text(Users[i].creationDate);
+                break;
+            case "#tabs-2":
+                var td3 = $("<td>").text(Users[i].deletionDate);
+                break;
+            default:
+                break;
+        }
+        switch (Users[i].role) {
+            case 2:
+                var td4 = $("<td class='role_high'>").text("Administrator");
+                break;
+
+            default:
+                var td4 = $("<td>").text("Benutzer");
+                break;
+
+        }
+
+        switch (location) {
+            case "#tabs-1":
+                var btn_del1 = $("<div class='dropdown'>");
+                var btn_del2 = $("<button type='button' class='btn dropdown-toggle' data-toggle='dropdown'>").text("Aktionen");
+                var btn_del3 = $("<div class='dropdown-menu'>");
+                var btn_del4 = $("<a class='dropdown-item' onclick='del_User()'>").text("Delete");
+                var btn_del5 = $("<a class='dropdown-item' onclick='new_Pass()'>").text("New Password");
+                break;
+            case "#tabs-2":
+                var react_btn = $("<button class='btn btn-block blockify' onclick='react_User()'>").text("Reaktivieren");
+                break;
+            default:
+                break;
+        }
+
+
+        td3.attr('id', Users[i].id);
+        switch (location) {
+            case "#tabs-1":
+                if (!Users[i].hasOwnProperty('deletionDate')) {
+                    $("#tbod1").append(tr);
+                    $(tr).append(td1, td2, td3, td4, btn_del1);
+                    $(btn_del1).append(btn_del2, btn_del3);
+                    $(btn_del3).append(btn_del4, btn_del5);
+                }
+                break;
+            case "#tabs-2":
+                if (Users[i].hasOwnProperty('deletionDate')) {
+                    $("#tbod2").append(tr);
+                    $(tr).append(td1, td2, td3, td4, react_btn);
+                }
+                break;
+            default:
+                break;
+        }
+
+    }
+}
+
+function create_Users() {
+    var search = $("<input type='text' placeholder='Suche nach Namen' id='uSearch' name='uSearch'>");
+    $("#tabs-1").append(search);
+    if ($("#sel1").val() == "Card Darstellung") {
+        create_Card();
+    } else {
+        var loc = ['#tabs-1', '#tabs-2'];
+        for (var i = 0; i < 2; i++) {
+            create_Tablestructure(loc[i]);
+            create_Table(loc[i]);
+        }
+    }
+}
+
+function create_Tablestructure(location) {
+    var table = $("<table class='table table-striped'>");
+    var thead = $("<thead>");
+    var tr = $("<tr>");
+    var first = $("<td>").text("Vorname");
+    var last = $("<td>").text("Nachname");
+    var role = $("<td>").text("Rolle");
+    var action = $("<td>").text("Aktionen");
+
+    switch (location) {
+        case "#tabs-1":
+            var tbody = $("<tbody id='tbod1'>");
+            var date = $("<td>").text("Erstellt am");
+            break;
+        case "#tabs-2":
+            var tbody = $("<tbody id='tbod2'>");
+            var date = $("<td>").text("Gelöscht am");
+            break;
+        default:
+            break;
+    }
+
+    $(location).append(table);
+    $(table).append(thead, tbody);
+    $(thead).append(tr);
+    $(tr).append(first, last, date, role, action);
+}
+
+function create_Error(Message) {
+    var main_div = $("<div class='alert alert-success alert-dismissible'>").text(Message);
+    var close_btn = $("<button type='button' class='close' data-dismiss='alert' onclick='update()'>").text("X");
+    $("#http-status").append(main_div);
+    $(main_div).append(close_btn);
+}
+
+function check_Equality() {
+    var password1 = $("#password").val();
+    var password2 = $("#password2").val();
+    if (password1 == password2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function clear_Form() {
+    $("#password").val('').blur();
+    $("#password2").val('').blur();
+    $("#email").val('').blur();
+    $("#vname").val('').blur();
+    $("#nname").val('').blur();
+}
+
+function add_User() {
+    if (!check_Equality()) {
+        create_Error("Passwort stimmt nicht überein.");
+        $("#password").val('').blur();
+        $("#password2").val('').blur();
+    } else {
+        var role;
+        if ($("#sel2").val() == "Administrator") {
+            role = 2;
+        } else {
+            role = 1;
+        }
+        $.ajax({
+            type: "POST",
+            url: "http://h2669567.stratoserver.net:8080/intranet/update.jsp?action=newuser",
+            data: {
+                email: $("#email").val(),
+                firstname: $("#vname").val(),
+                lastname: $("#nname").val(),
+                password: $("#password").val(),
+                role: role
+            },
+            success: function(msg) {
+                var main_div = $("<div class='alert alert-success'>").text("Benutzer erfolgreich erstellt!");
+                $("#http-status").append(main_div);
+            },
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                create_Error(errorThrown);
+            }
+        });
+        clear_Form();
+    }
+}
+
+function del_User(origin) {
+    var id = $(origin).parent().parent().find("h1").attr("id");
+    $.ajax({
+        type: "POST",
+        url: "http://h2669567.stratoserver.net:8080/intranet/update.jsp?action=deluser",
+        data: {
+            userid: id
+        },
+        success: function(response) {
+            positiv_Feedback(response);
+        }
+    });
+}
+
+function react_User(origin) {
+    var id = $(origin).parent().parent().find("h1").attr("id");
+    $.ajax({
+        type: "POST",
+        url: "http://h2669567.stratoserver.net:8080/intranet/update.jsp?action=actuser",
+        data: {
+            userid: id
+        },
+        success: function(response) {
+            positiv_Feedback(response);
+        }
+    });
+}
+
+function new_Pass(origin) {
+    var id = $(origin).parent().parent().find("h1").attr("id");
+    $.ajax({
+        type: "POST",
+        url: "http://h2669567.stratoserver.net:8080/intranet/update.jsp?action=newpass",
+        data: {
+            userid: id
+        },
+        success: function(response) {
+            positiv_Feedback(response);
+        }
+    });
+}
+
+function positiv_Feedback(response) {
+    if (response.status == "OK") {
+        var main_div = $("<div class='alert alert-success alert-dismissible'>").text("Aktion erfolgreich!");
+        var close_btn = $("<button type='button' class='close' data-dismiss='alert' onclick='update()'>").text("X");
+        $("#http-status").append(main_div);
+        $(main_div).append(close_btn);
+    } else {
+        create_Error(response.message);
+    }
+}
---
+++ user.jsp
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Intranet User Manager</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous" />
+
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+    <script src="js/functions.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1,shrink-to-fit=no" />
+    <link rel="stylesheet" href="css/intranet.css" />
+    <link rel="stylesheet" href="css/user.css" />
+</head>
+
+<body>
+    <div id="wrapper">
+
+        <div class="logo">
+            <a href="index.jsp"><img src="images/intranet_logo.jpg" alt="Intranet-Logo" /></a>
+        </div>
+
+        <nav id="tabs">
+            <ul class="nav nav-tabs">
+                <li><a class="nav-link" data-toggle="tab" href="#tabs-1">Aktive User</a></li>
+                <li><a class="nav-link" data-toggle="tab" href="#tabs-2">Inaktive Nutzer</a></li>
+                <li><a class="nav-link" data-toggle="tab" href="#tabs-3">Archiv</a></li>
+                <li><a class="nav-link" data-toggle="tab" href="#tabs-4">Neuen User Anlegen</a></li>
+                <li>
+                    <div class="form-group" onChange="update()">
+                        <select class="form-control" id="sel1">
+                              <option>Card Darstellung</option>
+                              <option>Tabelle Darstellung</option>
+                            </select>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="http-status">
+        </div>
+
+        <div class="tab-content">
+
+            <div id="tabs-1" class="tab-pane fade search_card">
+                <!-- <h2>Aktive User</h2> -->
+
+            </div>
+            <div id="tabs-2" class="tab-pane fade search_card">
+                <!-- <h2>Inaktive User</h2> -->
+
+            </div>
+            <div id="tabs-3" class="tab-pane fade search_card">
+                <!-- <h2>Archiv</h2> -->
+
+            </div>
+            <div id="tabs-4" class="tab-pane fade search_card">
+                <!-- <h2>Neuen User anlegen</h2> -->
+                <div class="form-group">
+                    <label for="email">E-Mail:</label>
+                    <input type="email" class="form-control" id="email" placeholder="username@example.de" data-toggle="tooltip" title="E-Mail Adresse" data-placement="bottom">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password:</label>
+                    <input type="password" class="form-control" id="password" placeholder="*****" data-toggle="tooltip" title="Passwort" data-placement="bottom">
+                    <label for="password2">Passwort wiederholen:</label>
+                    <input type="password" class="form-control" id="password2" placeholder="*****" data-toggle="tooltip" title="Passwort" data-placement="bottom">
+                </div>
+                <div class="form-group">
+                    <label for="vname">Vorname:</label>
+                    <input type="text" class="form-control" id="vname" placeholder="Max" data-toggle="tooltip" title="Vorname" data-placement="bottom">
+                </div>
+                <div class="form-group">
+                    <label for="nname">Nachname:</label>
+                    <input type="text" class="form-control" id="nname" placeholder="Mustermann" data-toggle="tooltip" title="Nachname" data-placement="bottom">
+                </div>
+                <div class="form-group">
+                    <label for="role">Rolle:</label>
+                    <select class="form-control" id="sel2" data-toggle="tooltip" title="Rolle" data-placement="bottom">
+                            <option>Benutzer</option>
+                            <option>Administrator</option>
+                        </select>
+                </div>
+
+                <button type="button" class="btn" onclick="add_User()">Submit</button>
+
+            </div>
+
+        </div>
+
+    </div>
+</body>
+
+</html>
